e2e5f9f09e770dc41bbc82d5e2581311c437f998
Shixin Zeng <zeng.shixin@gmail.com>
2012-02-11 18:48:37 +0100

    move the User Agent selection out of debug
    This helps the users on Tablets. Initially the UAString selection is
    only possible when the Browser is built with debug enabled, by this
    change, it is moved to advanced selection and available in the release
    version.
    
    Change-Id: If31e1e58be92e6b2e40ddf29bc98d4c4324f727d

--- src/com/android/browser/BrowserSettings.java
+++ src/com/android/browser/BrowserSettings.java
@@ -735,9 +735,6 @@ public class BrowserSettings implements OnSharedPreferenceChangeListener,
     }
 
     public int getUserAgent() {
-        if (!isDebugEnabled()) {
-            return 0;
-        }
         return Integer.parseInt(mPrefs.getString(PREF_USER_AGENT, "0"));
     }
 
