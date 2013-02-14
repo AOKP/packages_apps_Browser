47fbd4f87155a6e6a01d5b412264be8550f23936
kecinzer <kecinzer@gmail.com>
2013-02-14 13:14:32 +0100

    Merge Android 4.2.2
    Change-Id: I8ea4f270e4376f97328c6bc8892f3e2b0c29a722
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- src/com/android/browser/Controller.java
+++ src/com/android/browser/Controller.java
@@ -100,6 +100,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -2277,7 +2278,7 @@ public class Controller
          */
         private File getTarget(DataUri uri) throws IOException {
             File dir = mActivity.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
-            DateFormat format = new SimpleDateFormat(IMAGE_BASE_FORMAT);
+            DateFormat format = new SimpleDateFormat(IMAGE_BASE_FORMAT, Locale.US);
             String nameBase = format.format(new Date());
             String mimeType = uri.getMimeType();
             MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
